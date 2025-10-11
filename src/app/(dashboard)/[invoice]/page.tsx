33d73e16464b63967e7c92cbb79f586b7807ad82
Pedro Lima <pedrohblima03@gmail.com>
2025-10-10 21:34:50 -0300

    feat: first project structure

---
+++ src/app/(dashboard)/[invoice]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvoicePage = () => {
+  return (
+    <div>
+      InvoicePage
+    </div>
+  )
+}
+
+export default InvoicePage
---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      DashboardPage
+    </div>
+  )
+}
+
+export default DashboardPage
+
+
+
